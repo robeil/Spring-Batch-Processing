9dec6ccdf0bab4789934302843658e4b59b1fa7b
Robeil Aregawi <silukeen1@gmail.com>
2023-06-04 18:19:50 -0700

    spring batch processing

---
+++ src/main/java/com/robeil/springbatchprocessing/config/SpringBatchConfig.java
@@ -0,0 +1,96 @@
+package com.robeil.springbatchprocessing.config;
+
+import com.robeil.springbatchprocessing.entity.Customer;
+import com.robeil.springbatchprocessing.repository.CustomerRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.data.RepositoryItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+
+@Configuration
+@EnableBatchProcessing
+@AllArgsConstructor
+public class SpringBatchConfig {
+
+    private JobBuilderFactory jobBuilderFactory;
+    private StepBuilderFactory stepBuilderFactory;
+    private CustomerRepository customerRepository;
+
+    @Bean
+    public FlatFileItemReader<Customer> reader() {
+        FlatFileItemReader<Customer> itemReader = new FlatFileItemReader<Customer>();
+        itemReader.setResource(new FileSystemResource("src/main/resources/customers.csv"));
+        itemReader.setName("csvReader");
+        itemReader.setLinesToSkip(1);
+        itemReader.setLineMapper(lineMapper());
+
+        return itemReader;
+    }
+        private LineMapper<Customer> lineMapper(){
+            DefaultLineMapper<Customer> lineMapper = new DefaultLineMapper<Customer>();
+
+            DelimitedLineTokenizer delimitedLineTokenizer = new DelimitedLineTokenizer();
+            delimitedLineTokenizer.setDelimiter(",");
+            delimitedLineTokenizer.setStrict(false);
+            delimitedLineTokenizer.setNames("id","firstName","lastName","email","gender","contactNo","country","dob");
+
+            BeanWrapperFieldSetMapper<Customer> fieldSetMapper = new BeanWrapperFieldSetMapper<Customer>();
+            fieldSetMapper.setTargetType(Customer.class);
+
+            lineMapper.setLineTokenizer(delimitedLineTokenizer);
+            lineMapper.setFieldSetMapper(fieldSetMapper);
+
+            return lineMapper;
+        }
+
+        @Bean
+        public CustomerProcessor processor(){
+            return  new CustomerProcessor();
+        }
+
+        @Bean
+        public RepositoryItemWriter<Customer> writer(){
+            RepositoryItemWriter<Customer> writer = new RepositoryItemWriter<Customer>();
+            writer.setRepository(customerRepository);
+            writer.setMethodName("save");
+            return writer;
+        }
+
+        @Bean
+        public Step step1(){
+            return stepBuilderFactory
+                    .get("csv-step").<Customer, Customer>chunk(10)
+                    .reader(reader())
+                    .processor(processor())
+                    .writer(writer())
+                    .taskExecutor(taskExecutor())
+                    .build();
+        }
+
+        @Bean
+        public Job runJob(){
+        return jobBuilderFactory.get("importCustomers")
+                .flow(step1())
+                .end().build();
+        }
+
+        @Bean
+        public TaskExecutor taskExecutor(){
+            SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+            asyncTaskExecutor.setConcurrencyLimit(10);
+            return asyncTaskExecutor;
+        }
+}
---
+++ src/main/java/com/robeil/springbatchprocessing/contoller/JobController.java
@@ -0,0 +1,33 @@
+package com.robeil.springbatchprocessing.contoller;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.batch.core.repository.JobRestartException;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    private Job job;
+
+    @PostMapping("/importCustomers")
+    public void importCsvToDBJob(){
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addLong("startAt",System.currentTimeMillis())
+                .toJobParameters();
+        try {
+            jobLauncher.run(job,jobParameters);
+        }catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException |
+                JobParametersInvalidException e){
+            e.printStackTrace();
+        }
+    }
+}
